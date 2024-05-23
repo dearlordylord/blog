4942617fa141f24a540dfe05d3ccb7982062418f
Igor Loskutov <igor.loskutoff@gmail.com>
2024-05-22 20:30:49 -0400

    questionable cors stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,15 @@ import mdx from "@astrojs/mdx";
 
 import vercel from "@astrojs/vercel/static";
 
+const contentSecurityPolicy = [
+  // `default-src 'self'`,
+  // `img-src *`,
+  // `script-src 'self' 'unsafe-eval' 'unsafe-inline'`,
+  // `style-src 'self' 'unsafe-inline'`,
+  `connect-src 'self' api.umami.is cloud.umami.is`,
+  // `frame-ancestors 'self' ${frameAncestors}`,
+];
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.loskutoff.com',
@@ -16,5 +25,13 @@ export default defineConfig({
     ]
   },
   output: "static",
-  adapter: vercel()
+  adapter: vercel(),
+  server: {
+    headers: {
+      'Content-Security-Policy': contentSecurityPolicy
+        .join(';')
+        .replace(/\s{2,}/g, ' ')
+        .trim()
+    }
+  }
 });
